9171b181eeb56588fec6100693c8a1f8f7d8a465
cocoachou <123175470+cocoachou@users.noreply.github.com>
2024-08-21 01:30:42 +0900

    setting

---
+++ app/src/navigators/StackNavigator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
